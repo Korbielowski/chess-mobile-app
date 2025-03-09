efc989de71f1ac7c14e4ab138f2f4a0bea988927
Korbielowski <dawidstasinski12@gmail.com>
2025-03-09 13:47:17 +0100

    Rename file, remove commented lines

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,3 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const BoardWidget());
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final Board board = Board();
-
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       ),
-//       home: Scaffold(
-//         appBar: AppBar(title: const Text("Chess")),
-//         body: Container(
-//           alignment: Alignment.bottomCenter,
-//           child: board.build(),
-//         ),
-//       ),
-//     );
-//   }
-// }
