00158cddb5686bef476c9b11b54c5e896ed46736
Korbielowski <dawidstasinski12@gmail.com>
2025-05-05 23:18:12 +0200

    Add settings_screen.dart file

---
+++ lib/settings_screen.dart
@@ -0,0 +1,24 @@
+import 'package:chess/game_screen.dart';
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Route')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const GameScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
